127e353ace5eda19acc10d44e067f94c8f144a41
blocage <marrytheuser@gmail.com>
2024-10-27 12:32:39 +0400

    Deploy to GitHub Pages

--- src/galaxy/nodeDetails/templates/commonPackageTemplate.jsx
+++ src/galaxy/nodeDetails/templates/commonPackageTemplate.jsx
@@ -7,13 +7,15 @@ function commonPackageTemplate(model, link, linkText) {
       <div className='hidden-xs'>
         <div className='col-xs-6'>
           <a href={link} target='_blank'>
-            <h4 title=HERE3>HERE2</h4>
+            <h4 title={linkText}>{linkText}</h4>
           </a>
         </div>
       </div>
       <div className='visible-xs-block'>
         <div className='row info-block'>
-          <div className='col-xs-6 no-overflow'><a href={link} target="_blank">HEREE</a></div>
+          <div className='col-xs-6 no-overflow'><a href={link} target="_blank">{linkText}</a></div>
+          <div id={model.id} className='in-degree col-xs-3'>{model.inDegree}</div>
+          <div id={model.id} className='out-degree col-xs-3'>{model.outDegree}</div>
         </div>
       </div>
     </div>
